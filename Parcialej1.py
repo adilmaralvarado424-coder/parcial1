056e5bb3aa65e590623900868955b2095c1f496b
Adilmar Alvarado <adilmaralvarado424@gmail.com>
2026-04-18 18:42:14 -0600

    guia del parcial

--- Parcialej1.py
+++ Parcialej1.py
@@ -1,10 +1,11 @@
-precio = float(input("Ingrese precio: "))
-vip = input("¿Es miembro VIP? (Sí/No): ")
+# adilmar alvarado
+precio = float(input("Ingrese precio del videojuego: "))
+vip = input("¿Es miembro VIP? (Si/No): ")
 
 if precio >= 500:
-    precio = precio * 0.9
+    precio = precio * 0.9   # descuento del 10%
 
 if vip.lower() == "sí":
-    precio = precio * 0.85 
+    precio = precio * 0.85  # descuento adicional del 15%
 
-print("El total es:", precio)
+print("El precio final es:", precio)
